9b5df42f0bb1a3d1072be3db819b737eb9a63880
Tim LaTorre <timlatorre@gmail.com>
2017-11-11 15:52:04 -0500

    working basic routes

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const routes = require('./routes/routes.js');
+
+app.listen(port, function(){
+  console.log(`listening on port ${port}`);
+})
+
+app.use('/', routes);
+
